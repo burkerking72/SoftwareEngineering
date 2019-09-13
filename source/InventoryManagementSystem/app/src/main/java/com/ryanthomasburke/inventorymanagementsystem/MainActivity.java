9e0c0d4ebd30aaedd8401c22fb111f1b36dffeb7
burkerking72 <ryburke15@gmail.com>
2019-09-12 18:14:01 -0700

    added source

---
+++ source/InventoryManagementSystem/app/src/main/java/com/ryanthomasburke/inventorymanagementsystem/MainActivity.java
@@ -0,0 +1,57 @@
+package com.ryanthomasburke.inventorymanagementsystem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        main();
+    }
+
+    public void main(){
+        // Create a new item
+        Map<String, Object> item = new HashMap<>();
+        item.put("name", "Milk");
+        item.put("id", 12345);
+        item.put("quantity", 100);
+        item.put("price", 3.99);
+
+        // Add a new document with a generated ID
+        db.collection("inventory")
+                .add(item)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println("DocumentSnapshot added with ID: " +
+                                documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println( "Error adding document" + e);
+                    }
+                });
+    }
+
+}
